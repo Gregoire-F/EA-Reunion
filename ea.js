1316a533032e37aefe4739a9b7b64f6a97a2fc49
Gregoire-F <gregoire.frou@gmail.com>
2025-11-03 16:47:44 +0100

    Ajout du responsive img de card page EA

--- ea.js
+++ ea.js
@@ -25,10 +25,10 @@ function afficherEntreprises(entreprises) {
   entreprises.forEach(entreprise => {
     const acf = entreprise.acf || {};
     const card = document.createElement("div");
-    card.className = "max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-sm text-[clamp(0.75rem,3vw,1rem)]";
+    card.className = "max-w-sm p-3 flex flex-col justify-evenly bg-white border border-gray-200 rounded-lg shadow-sm text-[clamp(0.75rem,3vw,1rem)]";
 
     card.innerHTML = `
-      <img class="w-full h-auto object-cover rounded mb-2" src="${acf.o3.url}" alt="${acf.entreprises}"><img>
+      <img class="object-cover rounded mb-2 w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px]" src="${acf.o3.url}" alt="${acf.entreprises}"><img>
       <h3 class="text-[clamp(1rem,3vw,1.5rem)] font-semibold mb-2">${acf.entreprises}</h3>
       <p><strong>Activités :</strong> ${acf.activites || "Non précisé"}</p>
       <p><strong>Adresse :</strong> ${acf.adresse || "Non précisé"}</p>
